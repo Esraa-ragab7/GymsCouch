18771ccfca60f90b6a234eb9733c5e9581226c40
Esraa Ragab <esraa.ragab@seamlabs.com>
2019-09-03 01:28:54 +0200

    add rootViewController and splashScreenViewController.

---
+++ GymsCouch/Controller/RootViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RootViewController.swift
+//  GymsCouch
+//
+//  Created by Esraa Ragab on 9/3/19.
+//  Copyright © 2019 Esraa Ragab. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    
+}
+
---
+++ GymsCouch/Controller/SplashScreenViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SplashScreenViewController.swift
+//  GymsCouch
+//
+//  Created by Esraa Ragab on 9/3/19.
+//  Copyright © 2019 Esraa Ragab. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+    
+    
+}
