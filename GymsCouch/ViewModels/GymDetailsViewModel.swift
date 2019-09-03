98baf79b42956f0311470adb240174d8d3ef6331
esraa ragab <esraa.mo.ragab@gmail.com>
2019-09-03 23:27:43 +0200

    Add Gym Details View Model.

--- GymsCouch/ViewModels/GymDetailsViewModel.swift
+++ GymsCouch/ViewModels/GymDetailsViewModel.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2019 Esraa Ragab. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class GymDetailsViewModel {
+    
+    // MARK: - Properies
+    let gym: Gym
+    let image: String
+    let name: String
+    let description: String
+    let location: String
+    let time: String
+    let facilities: String
+    
+    // MARK: - Object Life Cycle
+    init(gym: Gym) {
+        self.gym = gym
+        self.name = gym.name!
+        self.image = gym.selectedImage!
+        self.description = gym.description!
+        self.location = gym.location?.address ?? ""
+        self.time = "from \(gym.opensAt ?? "") to \(gym.closesAt ?? "")"
+        self.facilities = gym.facilities!
+    }
+    
+}
