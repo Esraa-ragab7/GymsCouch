d91e361a19fcf3efc92acf560edb167723a17611
Esraa Ragab <esraa.ragab@seamlabs.com>
2019-09-03 20:29:14 +0200

    Complete home response model

--- GymsCouch/Model/HomeResponseModal.swift
+++ GymsCouch/Model/HomeResponseModal.swift
@@ -2,8 +2,23 @@
 //  HomeResponseModal.swift
 //  GymsCouch
 //
-//  Created by Abdelrahman Abu Sharkh on 9/3/19.
+//  Created by Esraa Ragab on 9/3/19.
 //  Copyright © 2019 Esraa Ragab. All rights reserved.
 //
 
 import Foundation
+
+class HomeResponseModal: Codable {
+    let code: Int
+    let status: String?
+    let data: Data?
+}
+
+extension HomeResponseModal {
+    
+    enum CodingKeys: String , CodingKey {
+        case code
+        case status
+        case data
+    }
+}
