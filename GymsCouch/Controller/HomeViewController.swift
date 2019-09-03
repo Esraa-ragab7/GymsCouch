2f4b11b602d7970d357ff37f0c55a5bfa25f56d0
Esraa Ragab <esraa.ragab@seamlabs.com>
2019-09-03 20:30:09 +0200

    Complete load data and home storyboard

--- GymsCouch/Controller/HomeViewController.swift
+++ GymsCouch/Controller/HomeViewController.swift
@@ -10,21 +10,71 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var gymsTabelView: UITableView!
+    
+    // MARK: - Properties
+    var gyms: [Gym] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureCollectionView()
+        callApi(page: 1, perPage: 4)
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - Tabel Data Source
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as! HomeTableViewCell
+        cell.displayData(description: gyms[indexPath.row].description ?? "", title: gyms[indexPath.row].name ?? "", imagePath: gyms[indexPath.row].images![0])
+        
+        return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gyms.count
+    }
+}
 
+// MARK: - Tabel View Delegate
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 238 + (gyms[indexPath.row].description?.height(withConstrainedWidth: tableView.frame.width, font: UIFont(name: "Gujarati Sangam MN", size: 16)!))!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+// MARK: - Private Functions
+extension HomeViewController {
+    
+    private func configureCollectionView() {
+        gymsTabelView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: "HomeTableViewCell")
+    }
+    
+    private func callApi(page: Int, perPage: Int) {
+        let queries: [Queries] = [
+            Queries(key: "page", value: "\(page)"),
+            Queries(key: "per_page", value: "\(perPage)")
+        ]
+        APIClient.getGyms(queries: queries) { (result) in
+            switch result {
+            case .success(let res):
+                if res.code == 200 {
+                    self.gyms = res.data?.items ?? []
+                    self.gymsTabelView.reloadData()
+                } else {
+                    print("Error")
+                }
+            case .failure(_):
+                print("Error")
+            }
+        }
+    }
+    
 }
