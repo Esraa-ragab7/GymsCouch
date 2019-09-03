eb67b67bbd36ffd4ad0fd8134abab397cf4b2e2d
Esraa Ragab <esraa.ragab@seamlabs.com>
2019-09-03 20:04:15 +0200

    add home table cell and home model and logout asset.

---
+++ GymsCouch/Controller/Cells/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  GymsCouch
+//
+//  Created by Abdelrahman Abu Sharkh on 9/3/19.
+//  Copyright © 2019 Esraa Ragab. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
